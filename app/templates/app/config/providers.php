371e5e5ea71e052cedf0402576108f61f0a5caca
Raffaele <izziaraffaele@gmail.com>
2015-04-19 16:51:47 +0200

    Fix twig path

--- app/templates/app/config/providers.php
+++ app/templates/app/config/providers.php
@@ -3,7 +3,7 @@
 return [
     'providers' => [
         ['class'=>'Silex\Provider\ServiceControllerServiceProvider'],
-        ['class'=>'Silex\Provider\TwigServiceProvider','value' => ['twig.path' => [ APPPATH.'/views',APPPATH.'/templates']]],
+        ['class'=>'Silex\Provider\TwigServiceProvider','values' => ['twig.path' => [ APPPATH.'/views',APPPATH.'/templates']]],
         ['class'=>'Silex\Provider\UrlGeneratorServiceProvider'],
         ['class'=>'MJanssen\Provider\RoutingServiceProvider'],
     ]
