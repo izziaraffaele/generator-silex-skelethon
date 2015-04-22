d5ccc85c2897f113eb5ad80ea7df24a5ae3e028a
Raffaele <izziaraffaele@gmail.com>
2015-04-22 13:30:46 +0200

    Fix testing suite

--- app/templates/tests/AppBundle/Tests/Controllers/HomeControllerTest.php
+++ app/templates/tests/AppBundle/Tests/Controllers/HomeControllerTest.php
@@ -2,14 +2,14 @@
 
 namespace AppBundle\Tests\Controllers;
 
-use WebComposer\TestCase;
+use AppBundle\Tests\WebTestCase as TestCase;
 
 class HomeControllerTest extends TestCase{
 
     public function testIndex()
     {
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/login');
+        $crawler = $client->request('GET', '/');
         $this->assertTrue($client->getResponse()->isOk());
         $this->assertCount(1, $crawler->filter('<h1>This is your homepage</h1>'));
     }
--- app/templates/tests/bootstrap.php
+++ app/templates/tests/bootstrap.php
@@ -3,8 +3,12 @@
  * Define environment
  * One of production,development,test
  */
-define( 'ENVIRONMENT', 'test' );
-define( 'BASEPATH', dirname(__DIR__) );
+define('ENVIRONMENT', 'test');
+define('BASEPATH', dirname(__DIR__));
+define('WEBPATH', __DIR__);
+define('APPPATH', BASEPATH.'/app');
+
+date_default_timezone_set ('Europe/Amsterdam');
 
 /**
  * Simple test harness for the time being, will delete it when I TDD out the 
